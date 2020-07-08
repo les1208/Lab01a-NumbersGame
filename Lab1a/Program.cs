c65310791ed15a35ca0d1beb85285ddc01479294
Lesley Rivera <les.aguilar@live.com>
2020-07-07 18:35:10 -0700

    Almost done

--- Lab1a/Program.cs
+++ Lab1a/Program.cs
@@ -6,7 +6,117 @@ namespace Lab1a
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Welcome to the numbers game!");
+                StartSequence();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public static void StartSequence()
+        {
+            try
+            {
+                Console.WriteLine("Pick a number larger than 0");
+                string answer = Console.ReadLine();
+                int number = Convert.ToInt32(answer);
+                int[] numbers = new int[number];
+                numbers = Populate(numbers);
+                int sum = GetSum(numbers);
+                int product = GetProduct(numbers, sum);
+                decimal quotient = GetQuotient(product);
+
+                Console.WriteLine($"Your array size is:{numbers.Length}");
+                Console.WriteLine($"The numbers in the array are {string.Join("", numbers)}");
+                Console.WriteLine($"The sum of the array is {sum}");
+                int chosenNumber = product / sum;
+                Console.WriteLine($"{sum} * {chosenNumber} =  {product}");
+                decimal mystery = product / quotient;
+                Console.WriteLine($"{product} / {mystery} = {quotient}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static int[] Populate(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                Console.WriteLine($"Enter Number: {i + 1} of {numbers.Length}");
+                string answer = Console.ReadLine();
+                int num = Convert.ToInt32(answer);
+                numbers[i] = num;
+            }
+            return numbers;
+        }
+
+        static int GetSum(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+                {
+                sum += numbers[i];
+            }
+            if (sum < 20)
+            {
+                throw new Exception($"Value of {sum} is low");
+            }
+            return sum;
+        }
+
+        public static int GetProduct(int[] numbers, int sum)
+        {
+            try
+            {
+                Console.WriteLine($"Select a random number between 1 and {numbers.Length}");
+                string answer = Console.ReadLine();
+                int random = Convert.ToInt32(answer);
+                int product = numbers[random - 1] * sum;
+                return product;
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+        }
+
+        public static decimal GetQuotient(int product)
+        {
+            try
+            {
+                Console.WriteLine($"Enter number to divide your product {product} by");
+                string answer = Console.ReadLine();
+                int number = Convert.ToInt32(answer);
+
+                decimal temp = decimal.Divide(product, number);
+
+                return temp;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+           
         }
     }
 }
+
---
+++ Lab1a/README.md
@@ -0,0 +1,53 @@
+# Lab01a-NumbersGame
+
+## Exception-Handling
+
+*Author: Lesley Rivera*
+
+----
+
+## Description
+This is a C# console application that will create a game that takes user input, manipulates data, and utilizes
+error handling, by following the given specifications.
+At the end of the game the user receives their total score.
+
+---
+
+### Getting Started
+Clone this repository to your local machine.
+
+```
+$ git clone [repo clone url here]
+```
+
+### To run the program from Visual Studio:
+Select ```File``` -> ```Open``` -> ```Project/Solution```
+
+Next navigate to the location you cloned the Repository.
+
+Double click on the ```Lab01-Numbers Game``` directory.
+
+Then select and open ```Lab1a.sln```
+
+---
+
+### Visuals
+***[Add screenshots of your application in action]***
+
+#### Application Start
+![Image 1](https://via.placeholder.com/750x500)
+#### Using the Application
+![Image 1](https://via.placeholder.com/750x500)
+#### Application End
+![Image 1](https://via.placeholder.com/750x500)
+
+---
+
+### Change Log 
+1.3: *Added summary comments to the methods* - 8 Nov 2010  
+1.2: *Changed variable names to follow proper convention* - 6 Nov 2010  
+1.1: *Added a Try/Catch/Finally for Question 2* - 5 Nov 2010  
+
+
+------------------------------
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
